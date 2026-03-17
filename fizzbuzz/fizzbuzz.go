8a4f18e79fd0b1208646d8bf1a43e5c23ca1b543
RinradaT <rinrada.t@arise.tech>
2026-03-17 12:22:12 +0700

    test mock Mod

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,6 +1,19 @@
 package fizzbuzz
 
-func FizzBuzz(n int) []string {
+import "fmt"
 
-	return []string{}
+type Moder interface {
+	Mod(x, y float64) float64
+}
+
+func FizzBuzz(mod Moder, n float64) (output []string) {
+
+	for i := 1.00; i <= n; i++ {
+		if mod.Mod(i, 15) != 0 {
+			fmt.Println("mod 15 is not 0")
+		}
+		output = append(output, "FizzBuzz")
+		continue
+	}
+	return
 }
--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -1,7 +1,28 @@
 package fizzbuzz_test
 
-import "testing"
+import (
+	"fizzbuzz/fizzbuzz"
+	"slices"
+	"testing"
+)
+
+type mockMod struct {
+	modValue float64
+}
+
+func (m *mockMod) Mod(x, y float64) float64 {
+	return m.modValue
+}
 
 func TestFizzBuzz(t *testing.T) {
-	t.Error("hello test")
+	mock := &mockMod{
+		modValue: 0,
+	}
+
+	modVal := fizzbuzz.FizzBuzz(mock, 1)
+
+	if !slices.Equal(modVal, []string{"FizzBuzz"}) {
+		t.Errorf("ExecQuery() returned %s, expected FizzBuzz", modVal)
+	}
+
 }
