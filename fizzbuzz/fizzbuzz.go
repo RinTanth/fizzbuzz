423cf19fdcb4dd36e0178bbb4299d5f0ee85f069
RinradaT <rinrada.t@arise.tech>
2026-03-23 09:55:22 +0700

    clear

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,28 +1,6 @@
 package fizzbuzz
 
-import (
-	"fmt"
-)
-
 func FizzBuzz(n int) (output []string) {
 
-	for i := 1; i <= n; i++ {
-		if i%15 != 0 {
-			if i%3 == 0 {
-				output = append(output, "Fizz")
-				continue
-			}
-			if i%5 == 0 {
-				output = append(output, "Buzz")
-				continue
-			}
-			output = append(output, fmt.Sprintf("%d", i))
-			continue
-		}
-		output = append(output, "FizzBuzz")
-		continue
-	}
-
-	fmt.Println(output)
 	return
 }
