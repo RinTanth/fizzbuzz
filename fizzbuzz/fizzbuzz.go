57c43eaa19602d6bc1a1cd22061e47fb60acfe1c
RinradaT <rinrada.t@arise.tech>
2026-03-25 10:58:59 +0700

    tc1

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,24 +1,6 @@
 package fizzbuzz
 
-import "strconv"
+func FizzBuzz(n int) (output string) {
 
-func FizzBuzz(n int) (output []string) {
-
-	for i := 1; i <= n; i++ {
-		if i%15 == 0 {
-			output = append(output, "FizzBuzz")
-			continue
-		}
-		if i%3 == 0 {
-			output = append(output, "Fizz")
-			continue
-		}
-		if i%5 == 0 {
-			output = append(output, "Buzz")
-			continue
-		}
-		output = append(output, strconv.Itoa(i))
-	}
-
-	return
+	return "1"
 }
